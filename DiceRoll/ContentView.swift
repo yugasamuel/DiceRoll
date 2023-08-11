cc8ea8c37b0c84295f805b70ff987cc1432e82e7
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-11 19:47:21 +0700

    Add: Dice result Text view Add: Dice roll button Add: Dice total result Text view Add: Customize dice button Add: Confirmation dialog for dice

--- DiceRoll/ContentView.swift
+++ DiceRoll/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rollResult = 1
+    @State private var totalResult = 1
+    @State private var totalSide = 4
+    @State private var isCustomizing = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("Total: \(totalResult)")
+                Spacer()
+                Button("Customize") {
+                    isCustomizing = true
+                }
+            }
+            .padding()
+            
+            Text("\(rollResult)")
+                .font(.largeTitle)
+            
+            Button(action: {
+                // add action
+            }, label: {
+                Text("Roll")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .padding(12)
+                    .frame(width: 125)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(8)
+            })
+            .padding(.bottom)
+            
+            List {
+                Text("A")
+            }
         }
-        .padding()
+        .toolbar {
+            
+        }
+        .confirmationDialog("Customize", isPresented: $isCustomizing) {
+            Button("4-sided") { totalSide = 4 }
+            Button("6-sided") { totalSide = 6 }
+            Button("8-sided") { totalSide = 8 }
+            Button("10-sided") { totalSide = 10 }
+            Button("12-sided") { totalSide = 12 }
+            Button("20-sided") { totalSide = 20 }
+            Button("100-sided") { totalSide = 100 }
+            Button("Cancel", role: .cancel) { }
+        }
     }
 }
 
