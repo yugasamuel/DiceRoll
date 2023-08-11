ec0543779f090237d14870bdc85588c1b2c1e5ee
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-11 22:13:20 +0700

    Add: Dice setting model

---
+++ DiceRoll/DiceSetting.swift
@@ -0,0 +1,13 @@
+//
+//  DiceSetting.swift
+//  DiceRoll
+//
+//  Created by Yuga Samuel on 11/08/23.
+//
+
+import Foundation
+
+struct DiceSetting: Codable {
+    var totalResult: Int
+    var totalSide: Int
+}
