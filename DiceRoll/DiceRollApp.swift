9e7feb6a69bae71272183f1bf7356209c97c1c42
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-11 19:12:23 +0700

    First commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Yuga Samuel on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
